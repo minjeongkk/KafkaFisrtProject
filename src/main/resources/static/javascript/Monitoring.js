861daaf2ae14591c94f438c686f4d3fb4ab249fb
minjeongkk <kminjeong01@naver.com>
2023-08-01 17:33:47 +0900

    모니터링 화면 구현

--- src/main/resources/static/javascript/Monitoring.js
+++ src/main/resources/static/javascript/Monitoring.js
@@ -1,21 +1,81 @@
-$.ajax({
-    url: "getAllTopic",
-    success: function (result) {
-        console.log(result);
-        $("#monitoringTable").empty();
-        var html = "";
-        var i = 0;
-        result.forEach(function (item) {
-            if (i % 2 == 0) {
-                html += "<div class='topic'>" + item.id + "</div>";
-            } else {
-                html += "<div class='topic'>" + item.id + "</div>";
+$(document).ready(function () {
+    $.ajax({
+        url: "getAllTopic",
+        success: function (result) {
+            console.log(result);
+            $("#monitoringTable").empty();
+            var html = "";
+            result.forEach(function (item) {
+                var setStatus = "STOPPED";
+                if (item.status == 'Running'){
+                    setStatus = "RUNNING";
+                }
+                html += "<div class='topic' id='" + item.id + "'>" +
+                    "<span class = 'monitoringName' id='" + item.id + "_monitoringName'>" + item.monitoringName + "</span>" +
+                    "<span class ='status' id='" + item.id + "_status'>" + setStatus + "</span>" +
+                    "<div class='row' id='buttons'>" +
+                    " <div class='col-auto'>" +
+                    "<button class='btn btn-primary' id='" + item.id + "_subscribe' type='button' onclick='subscribeTopic(this.id);'>구독</button>" +
+                    "<button class='btn btn-primary' id='" + item.id + "_stop' type='button' onclick='stopTopic(this.id);'>중지</button>" +
+                    "<button class='btn btn-primary' id='" + item.id + "_search' type='button' onclick='searchTopic(this.id);'>조회</button>" +
+                    "</div>" +
+                    "</div>" +
+                    "<table class='table' id='MonitoringListTable'>" +
+                    "<thead class='thead-light'>" +
+                    "<tr class='text-center'>" +
+                    "<th scope='col'>No.</th>" +
+                    "<th scope='col'>Col</th>" +
+                    "</tr>" +
+                    "</thead>" +
+                    "<tbody id='" + item.id + "_listArea'>" +
+                    "</tbody>" +
+                    "</table>" +
+                    "</div>";
+                if(setStatus=="RUNNING"){
+                    $("#"+item.id+"_status").css("color","green");
+                }
+            })
+            $("#monitoringTable").append(html);
+        }
+    })
+});
+
+function subscribeTopic(id){
+    var sendId = parseInt(id.split('_')[0])
+    $.ajax({
+        url: "subscribe/"+sendId,
+        success: function (result) {
+            console.log(result);
+            console.log("#"+sendId+"_status");
+            $("#"+sendId+"_status").css("color","green");
+            $("#"+sendId+"_status").text("RUNNING");
+        }
+    })
+}
+
+function stopTopic(id){
+    var sendId = parseInt(id.split('_')[0])
+    $.ajax({
+        url: "stop/"+sendId,
+        success: function (result) {
+            console.log(result);
+            $("#"+sendId+"_status").css("color","red");
+            $("#"+sendId+"_status").text("STOPPED");
+        }
+    })
+}
+
+function searchTopic(id){
+    var sendId = parseInt(id.split('_')[0])
+    $.ajax({
+        url: "getData/"+sendId,
+        success: function (result) {
+            console.log(result);
+            var html = "";
+            for(var i = 0; i < result.length; i++) {
+                html += "<tr> <td> # </td><td>" + result[i] + "</td></tr>";
             }
-            i += 1;
-            // html += "<div class='topic'> item.id </div>";
-            // html += "<tr onclick='tableClick(" + item.id + ")' id='" + item.id + "'> <td>" + item.topicName + "</td><td>"
-            //     + item.monitoringName + "</td><td>" + item.ip + "</td><td>" + item.port + "</td><td>" + item.status + "</td></tr>";
-        })
-        $("#monitoringTable").append(html);
-    }
-})
+            $("#"+sendId+"_listArea").append(html);
+        }
+    })
+}
