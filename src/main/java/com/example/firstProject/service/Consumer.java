823ba1597ee6138a44ce1b5a8895e55a78feb4c0
minjeongkk <kminjeong01@naver.com>
2023-08-07 14:46:57 +0900

    카프카 컨슈머 코드 수정

--- src/main/java/com/example/firstProject/service/Consumer.java
+++ src/main/java/com/example/firstProject/service/Consumer.java
@@ -9,7 +9,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.*;
 
-public class ConsumerTest {
+public class Consumer {
     private static String bootstrapServerIp = "";
     private static String bootstrapServerPort = "";
     private static String topicName = "";
@@ -28,26 +28,15 @@ public class ConsumerTest {
         // consumer properties
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerIp+":"+bootstrapServerPort);
-//        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID+id.toString());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-//        properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG,("SchedulerCoordinator"+UUID.randomUUID()));
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID+id.toString());
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-//        properties.setProperty("enable.auto.commit", "true");
-//        properties.setProperty("auto.commit.interval.ms", "1000");
-//        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
-
-//        if(consumer==null){
-//            // consumer 객체 생성, 지정한 토픽에서 데이터 받아옴
-//            consumer = new KafkaConsumer<>(properties);
-//            topics = new ArrayList<>();
-//        }
-        //        topics.add(topicName);
 
         consumer = new KafkaConsumer<>(properties);
         consumers.put(id, consumer);
         consumer.subscribe(Arrays.asList(topicName));
+        ConsumerRecords<String, String> records = consumers.get(id).poll(Duration.ofSeconds(1));
     }
 
     // 조회
